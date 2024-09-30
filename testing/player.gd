6a82edb3d107fb0f8199fe27df5f3467e5b8ef02
Sayer K <sklein5890@student.pps.net>
2024-09-30 11:31:25 -0700

    8d movement

---
+++ testing/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed = 500
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector:
+		velocity = input_vector * speed
+	else:
+		velocity = input_vector
+	
+	move_and_slide()
